6185287742971985545bdd46d996de77aa8fb7f0
lagux <nguyenhientrungnam@gmail.com>
2025-03-02 16:06:50 +0700

    update loading for movie details

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,22 +1,35 @@
 import { useParams } from "react-router-dom";
 import api from "../config/axios/axios";
 import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
 
 const MovieDetails = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchMovieDetails = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
     try {
       const endpoint = `movie/${id}`;
       const response = await api.get(endpoint);
       const data = response.data;
 
       console.log(data);
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Error fetching movies from the API");
+        return;
+      }
 
       setMovie(data || []);
     } catch (error) {
       console.log(error);
+      setErrorMessage("Error fetching movies from the API");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -33,20 +46,26 @@ const MovieDetails = () => {
   console.log(id);
   return (
     <div className="min-h-screen flex justify-center items-center p-6">
-      <div className="max-w-7xl w-full bg-[#0F0D23] text-white p-6 rounded-2xl shadow-lg">
-        {/* Header */}
-        <div className="flex justify-between items-center p-4 text-white">
-          <div>
-            <h1 className="text-3xl font-bold">{movie.title}</h1>
-            <p className="text-gray-400">
-              {movie.release_date ? movie.release_date.split("-")[0] : "N/A"}
-              <span> • </span>
-              {movieRuntime(movie.runtime)}
-            </p>
+      {isLoading ? (
+        <Spinner />
+      ) : errorMessage ? (
+        <p className="text-red-500">{errorMessage}</p>
+      ) : (
+        <div className="max-w-7xl w-full bg-[#0F0D23] text-white p-6 rounded-2xl shadow-lg">
+          {/* Header */}
+          <div className="flex justify-between items-center p-4 text-white">
+            <div>
+              <h1 className="text-3xl font-bold">{movie.title}</h1>
+              <p className="text-gray-400">
+                {movie.release_date ? movie.release_date.split("-")[0] : "N/A"}
+                <span> • </span>
+                {movieRuntime(movie.runtime)}
+              </p>
+            </div>
+            <div></div>
           </div>
-          <div>Phần bên phải</div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
