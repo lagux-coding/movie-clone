81ec756dbccdf170d9b7897f2204137c4a6118c7
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-03-03 01:10:00 +0700

    update full movie details page

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import Spinner from "./Spinner";
 import ReactPlayer from "react-player";
 import { FaPlay } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { format } from "date-fns";
 
 const YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v=";
 
@@ -12,6 +14,11 @@ const MovieDetails = () => {
   const [movie, setMovie] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
   const fetchMovieDetails = async () => {
     setIsLoading(true);
@@ -47,6 +54,13 @@ const MovieDetails = () => {
     return `${hours}h ${mins}m`;
   };
 
+  const formatDate = (date) => {
+    if (!date) return "N/A";
+
+    const formattedDate = formatter.format(new Date(date));
+    return formattedDate;
+  };
+
   console.log(id);
   return (
     <div className="min-h-screen flex justify-center items-center p-6">
@@ -58,15 +72,14 @@ const MovieDetails = () => {
         <div className="movie-details">
           {/* Header */}
           <div className="header">
-            <div className="header-left">
-              <h1>{movie.title}</h1>
+            <h1>{movie.title}</h1>
+            <div className="small-detail">
               <span>
                 {movie.release_date ? movie.release_date.split("-")[0] : "N/A"}
                 <span> • </span>
                 {movieRuntime(movie.runtime)}
               </span>
-            </div>
-            <div className="header-right">
+
               <div className="rating">
                 <img src="/star.svg" alt="star" />
                 <p>
@@ -106,6 +119,90 @@ const MovieDetails = () => {
                 <FaPlay />
                 Trailer
               </div>
+            </div>
+          </div>
+          {/* Overview */}
+          <div className="movie-overview">
+            <div className="metadata">
+              <div className="meta-item genre">
+                <span>Genre</span>
+                <ul>
+                  {movie.genres?.map((genre) => (
+                    <li key={genre.id}>{genre.name}</li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="meta-item">
+                <span>Overview</span>
+                <p>{movie.overview}</p>
+              </div>
+
+              <div className="meta-item">
+                <span>Release date</span>
+                <p>{formatDate(movie.release_date)}</p>
+              </div>
+
+              <div className="meta-item">
+                <span>Countries</span>
+                <ul>
+                  {movie.production_countries?.map((country, index) => (
+                    <li key={index}>
+                      {index > 0 && `•`} {country.name || "N/A"}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="meta-item">
+                <span>Status</span>
+                <p>{movie.status}</p>
+              </div>
+
+              <div className="meta-item">
+                <span>Language</span>
+                <ul>
+                  {movie.spoken_languages?.map((lang, index) => (
+                    <li key={lang.iso_639_1}>
+                      {index > 0 && `•`} {lang.english_name || "N/A"}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="meta-item">
+                <span>Budget</span>
+                <p>${movie.budget}</p>
+              </div>
+
+              <div className="meta-item">
+                <span>Revenue</span>
+                <p>${movie.revenue}</p>
+              </div>
+
+              <div className="meta-item">
+                <span>Tagline</span>
+                <p>{movie.tagline}</p>
+              </div>
+
+              <div className="meta-item">
+                <span>Production Companies</span>
+                <ul>
+                  {movie.production_companies?.map((company, index) => (
+                    <li key={company.id}>
+                      {index > 0 && `•`} {company.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="homepage">
+              <a href={movie.homepage} target="_blank" rel="noreferrer">
+                <span>Visit Homepage</span>
+                <span>
+                  <FaArrowRightLong />
+                </span>
+              </a>
             </div>
           </div>
         </div>
